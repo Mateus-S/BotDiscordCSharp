b8ce9b7182d3d834b77009d651736ad76973453d
Mateus-S <mateus.gcr.tog@hotmail.com>
2021-12-04 11:21:54 -0300

    inicio

---
+++ BotCSharpDiscord/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace BotCSharpDiscord
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+			=> new Program().MainAsync().GetAwaiter().GetResult();
+
+
+		private DiscordSocketClient _client;
+		public async Task MainAsync()
+		{
+			_client = new DiscordSocketClient();
+
+			_client.Log += Log;
+
+			var token = "";
+
+			await _client.LoginAsync(TokenType.Bot, token);
+			await _client.StartAsync();
+
+			await Task.Delay(-1);
+		}
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
